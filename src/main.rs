21ef00a8c1a0cf0e4edf0b683df50b99f26d81c0
Nils Martel <nils.martel@graphmasters.net>
2020-06-11 10:16:17 +0200

    More commands

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
+use itertools::join;
 use std::io::Read;
 use structopt::StructOpt;
 fn main() {
     let config: StringCommand = StringCommand::from_args();
-
-    let input = {
-        let mut buffer = String::new();
-        std::io::stdin()
-            .read_to_string(&mut buffer)
-            .expect("Failed to read from stdin");
-        buffer
-    };
+    let input = stdin_as_string();
 
     use StringCommand::*;
     match config {
         Substr { start, end } => {
-            // please don't do this at home
+            // please don't do this at home. Byte indexing isn't okay.
             println!("{}", &input[start..end]);
         }
-        Split { pattern } => {
-            for line in input.split(&pattern) {
-                // don't do this, it's unperformant and people will know you're bad at rust
+        Split { separator } => {
+            for line in input.split(&separator) {
+                // performs poorly, locks stdout over and over again
                 println!("{}", line);
             }
         }
+        Length => println!("{}", input.len()),
+        Replace { matching, with } => {
+            let result = join(input.split(&matching), &with);
+            println!("{}", result);
+        }
     }
+}
+
+fn stdin_as_string() -> String {
+    let mut buffer = String::new();
+    std::io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("failed to read stdin to string.");
+    buffer
 }
 
 #[derive(StructOpt, Debug)]
@@ -36,9 +43,17 @@ enum StringCommand {
         #[structopt(short, long)]
         end: usize,
     },
-    /// Split up a string by a pattern and print the parts on separate lines
+    /// Split up a string by a separator and print the parts on separate lines
     Split {
         #[structopt(default_value = " ", short)]
-        pattern: String,
+        separator: String,
+    },
+    /// Returns the length of string
+    Length,
+    Replace {
+        #[structopt(short, long = "match")]
+        matching: String,
+        #[structopt(short, long)]
+        with: String,
     },
 }
