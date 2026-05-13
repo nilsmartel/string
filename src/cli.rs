30982c221ce124c69509352433164207f98638df
Nils Martel <nilsmartel@yahoo.de>
2026-05-13 14:50:10 +0200

    Refactor code

---
+++ src/cli.rs
@@ -0,0 +1,91 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand, Debug)]
+pub enum CaseStyle {
+    /// lowercase
+    Lower,
+    /// UPPERCASE
+    Upper,
+}
+
+#[derive(Parser, Debug)]
+#[command(about = "Cli for common string operations. Takes input from stdin.")]
+pub enum StringCommand {
+    /// Transform upper- or lowercase
+    #[command(subcommand)]
+    Case(CaseStyle),
+    /// Reverse order of lines
+    Reverse,
+    /// Extract part of a given string.
+    Substr {
+        #[arg()]
+        start: usize,
+        #[arg()]
+        end: usize,
+    },
+    /// Split up a string by a separator and print the parts on separate lines
+    Split {
+        #[arg(default_value = " ")]
+        separator: String,
+    },
+    /// Returns the length the input string
+    Length,
+    /// Replace all matching characters
+    Replace {
+        #[arg()]
+        matching: String,
+        #[arg()]
+        with: String,
+    },
+    /// Pick a single line by index
+    Line {
+        #[arg()]
+        /// starting at 0
+        number: usize,
+    },
+    /// Interleave input and only print every nth line
+    Interleave {
+        #[arg()]
+        /// starting at 0
+        n: usize,
+    },
+    /// Output the set of input strings without repetitions, in order
+    Distinct {
+        #[arg(short)]
+        /// Distinct entire lines, instead of individual words
+        lines: bool,
+    },
+    /// Trim whitespace on lines and ignore empty ones
+    Trim,
+    /// Prints all chars on separate lines
+    Chars,
+    /// Useful for templating, replace sections of input with the output of a shell command or script
+    Template {
+        #[arg(default_value = "{{", long = "begin")]
+        /// Delimiter indicating beginning of command
+        begin: String,
+
+        #[arg(default_value = "}}", long = "end")]
+        /// Delimiter indicating end of command
+        end: String,
+
+        #[arg(default_value = "sh", long)]
+        /// in which shell the commands should be piped
+        shell: Vec<String>,
+
+        #[arg(long = "raw-output")]
+        /// don't trim new lines and whitespace of the start and end of output
+        raw_output: bool,
+    },
+    /// Map each line of input to a subcommand.
+    Each {
+        /// If set, input will be passed as stdin
+        #[arg(short = 's', long = "stdin", default_value_t = false)]
+        stdin: bool,
+        /// Name of value to be replaced. Default is ""
+        #[arg(short = 'v', long = "var", default_value = "{}")]
+        var: String,
+        /// Command to be executed. Pass "string each -- <commands...>" so you can pass flags to the command.
+        command: Vec<String>,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,16 @@
+mod cli;
 mod exec;
 mod templating;
 mod util;
 
 use templating::template;
 
-use clap::{Parser, Subcommand};
 use itertools::join;
 
 use crate::exec::{execute, execute_command};
 
-#[derive(Subcommand, Debug)]
-enum CaseStyle {
-    /// lowercase
-    Lower,
-    /// UPPERCASE
-    Upper,
-}
-
-#[derive(Parser, Debug)]
-#[command(about = "Cli for common string operations. Takes input from stdin.")]
-enum StringCommand {
-    /// Transform upper- or lowercase
-    #[command(subcommand)]
-    Case(CaseStyle),
-    /// Reverse order of lines
-    Reverse,
-    /// Extract part of a given string.
-    Substr {
-        #[arg()]
-        start: usize,
-        #[arg()]
-        end: usize,
-    },
-    /// Split up a string by a separator and print the parts on separate lines
-    Split {
-        #[arg(default_value = " ")]
-        separator: String,
-    },
-    /// Returns the length the input string
-    Length,
-    /// Replace all matching characters
-    Replace {
-        #[arg()]
-        matching: String,
-        #[arg()]
-        with: String,
-    },
-    /// Pick a single line by index
-    Line {
-        #[arg()]
-        /// starting at 0
-        number: usize,
-    },
-    /// Interleave input and only print every nth line
-    Interleave {
-        #[arg()]
-        /// starting at 0
-        n: usize,
-    },
-    /// Output the set of input strings without repetitions, in order
-    Distinct {
-        #[arg(short)]
-        /// Distinct entire lines, instead of individual words
-        lines: bool,
-    },
-    /// Trim whitespace on lines and ignore empty ones
-    Trim,
-    /// Prints all chars on separate lines
-    Chars,
-    /// Useful for templating, replace sections of input with the output of a shell command or script
-    Template {
-        #[arg(default_value = "{{", long = "begin")]
-        /// Delimiter indicating beginning of command
-        begin: String,
-
-        #[arg(default_value = "}}", long = "end")]
-        /// Delimiter indicating end of command
-        end: String,
-
-        #[arg(default_value = "sh", long)]
-        /// in which shell the commands should be piped
-        shell: Vec<String>,
-
-        #[arg(long = "raw-output")]
-        /// don't trim new lines and whitespace of the start and end of output
-        raw_output: bool,
-    },
-    /// Map each line of input to a subcommand.
-    Each {
-        /// If set, input will be passed as stdin
-        #[arg(short = 's', long = "stdin", default_value_t = false)]
-        stdin: bool,
-        /// Name of value to be replaced. Default is ""
-        #[arg(short = 'v', long = "var", default_value = "{}")]
-        var: String,
-        /// Command to be executed. Pass "string each -- <commands...>" so you can pass flags to the command.
-        command: Vec<String>,
-    },
-}
-
 fn main() -> std::io::Result<()> {
-    let command: StringCommand = StringCommand::parse();
+    let command: cli::StringCommand = StringCommand::parse();
     let input = util::stdin_as_string();
     let mut output = std::io::stdout();
 
@@ -109,7 +19,7 @@ fn main() -> std::io::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use super::{perform_command, StringCommand, StringCommand::*};
+    use super::{cli::StringCommand, cli::StringCommand::*, perform_command};
     use std::fmt::Formatter;
 
     struct TestWriter {
